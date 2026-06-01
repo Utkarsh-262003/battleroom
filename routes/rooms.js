ef84331f9662215b394a7d0a544c94490d961b01
Utkarsh-262003 <utkarshtyagi9050@gmail.com>
2026-06-01 19:15:09 +0000

    add Room model and create route

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,9 +1,18 @@
 const express = require('express') 
 const router  = express.Router() 
 const authMiddleware = require('../middleware/auth.js')
-
+const Room = require('../models/Room')
   
-router.get('/', authMiddleware, (req, res) => { res.json({ rooms: 'List of Rooms' })     }) 
-router.post('/create', authMiddleware, (req, res) => { res.json({ message: 'Room Created' })    }) 
-  
+router.get('/', authMiddleware,  (req, res) => { res.json({ rooms: 'List of Rooms' })     }) 
+router.post('/create', authMiddleware, async(req, res) => {
+    try {
+        const name = req.body.name
+        const host = req.user._id
+       const room = await Room.create({ name, host}) 
+        res.json({ message: 'Room Created Successfully' })
+}
+catch(err){
+    res.status(500).json({error : 'Internal Server Error'})
+}
+})
 module.exports = router
