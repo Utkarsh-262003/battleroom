17b91f3263d0747b9079503c490d7c300e2f145f
Utkarsh-262003 <utkarshtyagi9050@gmail.com>
2026-06-09 18:51:47 +0000

    render issue

--- app.js
+++ app.js
@@ -92,6 +92,7 @@ function sendQuestion(roomId){
 }
 const { GoogleGenerativeAI } = require('@google/generative-ai')
 
+
 async function fetchQuestions() {
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
   const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
